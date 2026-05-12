132dc781f86c8a846b0109911ac5fd141dc9acee
Rish <gulati.r7121@gmail.com>
2026-05-12 14:09:39 +0000

    github actions pipeline

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const memes = [
   "https://i.imgflip.com/30b1gx.jpg",
@@ -9,6 +9,18 @@ const memes = [
 
 function App() {
   const [image, setImage] = useState(memes[0]);
+  const [message, setMessage] = useState("Loading backend...");
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch(() => {
+        setMessage("Backend sleeping 😴");
+      });
+  }, []);
 
   const chaosMode = () => {
     const randomImage =
@@ -31,10 +43,12 @@ function App() {
       }}
     >
       <div style={{ textAlign: "center" }}>
-        <h1>😂 Cursor Chaos App</h1>
+        <h1>😂 Cursor Chaos DevSecOps App</h1>
+
+        <h2>{message}</h2>
 
         <p>
-          Move your cursor and witness engineering-grade nonsense 🚀
+          Move cursor for meme chaos 🚀
         </p>
 
         <img
@@ -52,3 +66,7 @@ function App() {
 }
 
 export default App;
+
+
+
+
